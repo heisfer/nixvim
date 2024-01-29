a1af036cd9cdc75687ae8d79636429210f779009
heisfer <heisfer@refract.dev>
2024-01-29 12:19:17 +0200

    Init: Plugins/Telescope

--- config/default.nix
+++ config/default.nix
@@ -2,8 +2,10 @@
   # Import all your configuration modules here
   imports = [
     ./options.nix
+		./globals.nix
     ./plugins/lualine.nix
 		./plugins/git.nix
+		./plugins/telescope.nix
   ];
 
   colorschemes.rose-pine = {
---
+++ config/plugins/telescope.nix
@@ -0,0 +1,24 @@
+{
+  plugins.telescope = {
+    enable = true;
+
+    keymaps = {
+      "<leader>ff" = "find_files";
+      "<leader>fg" = "live_grep";
+      "<leader>fb" = "buffers";
+      "<leader>fh" = "help_tags";
+      "<leader>fd" = "diagnostics";
+    };
+
+    defaults = {
+      file_ignore_patterns = [
+        "^.git/"
+        "^.mypy_cache/"
+        "^__pycache__/"
+        "^output/"
+        "^data/"
+        "%.ipynb"
+      ];
+    };
+  };
+}
