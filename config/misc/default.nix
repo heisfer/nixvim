35d6618f8747d748c05e3264260a29d51db23b55
heisfer <heisfer@aol.com>
2024-04-13 15:10:45 +0300

    twilight/markdown-preview/direnv plugin added

--- config/misc/default.nix
+++ config/misc/default.nix
@@ -2,11 +2,13 @@
   imports = [
     ./extra.nix
     ./which-key.nix
+    ./markdown-preview.nix
   ];
   plugins = {
     nvim-autopairs = {
       enable = true;
     };
     surround.enable = true;
+    direnv.enable = true;
   };
 }
--- config/ui/default.nix
+++ config/ui/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./alpha.nix
     ./lualine.nix
+    ./twilight.nix
   ];
 }
