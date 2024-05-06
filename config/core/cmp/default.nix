b7983eadbcb8b46df2223930dfe86e10cf0ac946
heisfer <heisfer@aol.com>
2024-05-06 17:50:01 +0300

    cmp keymap change

--- config/core/cmp/default.nix
+++ config/core/cmp/default.nix
@@ -18,8 +18,8 @@
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-e>" = "cmp.mapping.close()";
-          "<C-p>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's' })";
-          "<C-n>" = "cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })";
+          "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's' })";
+          "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })";
           "<C-y>" = "cmp.mapping.confirm({ select = true })";
         };
         snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
